685403e08959d50be7286b11341e3377d0ca75cb
William Griffiths <willjgriff@gmail.com>
2017-09-22 16:06:26 +0100

    Updated the way promises are used in tests. More concise now.

--- test/DelegationProxyTest.js
+++ test/DelegationProxyTest.js
@@ -10,40 +10,39 @@ contract("DelegationProxy", accounts => {
     const delegateToAccount = accounts[1]
     const delegateFromAccount = accounts[0]
 
-    beforeEach(() => {
-        return DelegationProxy.new(0)
-            .then(_delegationProxy => delegationProxy = _delegationProxy)
+    beforeEach(async () => {
+        delegationProxy = await DelegationProxy.new(0)
     })
 
     describe("delegate(address _to)", () => {
 
-        it("creates Delegate Log event", () => {
-            return delegationProxy.delegate(delegateToAccount, {from: delegateFromAccount})
-                .then(tx => TestUtils.listenForEvent(delegationProxy.Delegate()))
-                .then(delegateArgs => {
-                    assert.equal(delegateArgs.who, delegateFromAccount, "Delegate Log shows delegating from isn't sender")
-                    assert.equal(delegateArgs.to, delegateToAccount, "Delegate Log shows delegating to isn't passed address")
-                })
+        it("creates Delegate Log event", async () => {
+            const tx = await delegationProxy.delegate(delegateToAccount, {from: delegateFromAccount})
+            const delegateArgs = await TestUtils.listenForEvent(delegationProxy.Delegate())
+
+            assert.equal(delegateArgs.who, delegateFromAccount, "Delegate Log shows delegating from isn't sender")
+            assert.equal(delegateArgs.to, delegateToAccount, "Delegate Log shows delegating to isn't passed address")
         })
 
-        it("updates delegations mapping with new delegate", () => {
-            return delegationProxy.delegate(delegateToAccount, {from: delegateFromAccount})
-                .then(tx => delegationProxy.delegations(delegateFromAccount, 0))
-                .then(delegations => {
-                    assert.equal(delegations[0], web3.eth.blockNumber, "Delegations block number is incorrect")
-                    assert.equal(delegations[1], delegateToAccount, "Delegations to account is incorrect")
-                    // TODO: The below is incorrect, delegations function needs completing
-                    assert.equal(delegations[2], 0, "Delegations toIndex is incorrect")
-                })
+        it("updates delegations mapping with new delegate", async () => {
+            const tx = await delegationProxy.delegate(delegateToAccount, {from: delegateFromAccount})
+            const delegations = await delegationProxy.delegations(delegateFromAccount, 0)
+
+            assert.equal(delegations[0], web3.eth.blockNumber, "Delegations block number is incorrect")
+            assert.equal(delegations[1], delegateToAccount, "Delegations to account is incorrect")
+            // TODO: The below is incorrect, delegations function needs completing
+            assert.equal(delegations[2], 0, "Delegations toIndex is incorrect")
+
         })
     })
 
     describe("delegatedToAt(address _who, uint _block)", () => {
 
-        it("returns correctly delegated to address", () => {
-            return delegationProxy.delegate(delegateToAccount, {from: delegateFromAccount})
-                .then(() => delegationProxy.delegatedToAt.call(delegateFromAccount, web3.eth.blockNumber))
-                .then(delegatedTo => assert.equal(delegatedTo, delegateToAccount, "Delegated to account is incorrect"))
+        it("returns correctly delegated to address", async () => {
+            await delegationProxy.delegate(delegateToAccount, {from: delegateFromAccount})
+            const delegatedTo = await delegationProxy.delegatedToAt.call(delegateFromAccount, web3.eth.blockNumber)
+
+            assert.equal(delegatedTo, delegateToAccount, "Delegated to account is incorrect")
         })
     })
 
@@ -52,22 +51,17 @@ contract("DelegationProxy", accounts => {
         let miniMeTokenFactory, miniMeToken
         const accountBalance = 1000
 
-        beforeEach(() => {
-            return MiniMeTokenFactory.new()
-                .then(_miniMeTokenFactory => {
-                    miniMeTokenFactory = _miniMeTokenFactory
-                    return MiniMeToken.new(miniMeTokenFactory.address, 0, 0, "TestToken", 18, "TTN", true)
-                })
-                .then(_miniMeToken => {
-                    miniMeToken = _miniMeToken
-                    return miniMeToken.generateTokens(delegateFromAccount, accountBalance)
-                })
+        beforeEach(async () => {
+            miniMeTokenFactory = await MiniMeTokenFactory.new()
+            miniMeToken = await MiniMeToken.new(miniMeTokenFactory.address, 0, 0, "TestToken", 18, "TTN", true)
+            await miniMeToken.generateTokens(delegateFromAccount, accountBalance)
         })
 
-        it("returns expected amount of influence", () => {
-            return delegationProxy.delegate(delegateToAccount, {from: delegateFromAccount})
-                .then(() => delegationProxy.delegatedInfluenceFromAt(delegateFromAccount, miniMeToken.address, web3.eth.blockNumber))
-                .then(delegatedInfluence => assert.equal(delegatedInfluence.toNumber(), 1000, "Delegated influence is not as expected"))
+        it("returns expected amount of influence", async () => {
+            await delegationProxy.delegate(delegateToAccount, {from: delegateFromAccount})
+            const delegatedInfluence = await delegationProxy.delegatedInfluenceFromAt(delegateToAccount, miniMeToken.address, web3.eth.blockNumber)
+
+            assert.equal(delegatedInfluence.toNumber(), 1000, "Delegated influence is not as expected")
         })
     })
 })
